3845d41912f724e130f292057ba791062ab9544c
satells <msergiost@hotmail.com>
2022-09-02 14:13:02 -0300

    Alteração de mensagem

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 
 
 console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Rodando o sistema de gerenciamento de jogos");
